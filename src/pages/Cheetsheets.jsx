4d9678af6c4bbcbf4595cd800bf59e119f07830b
sofia_lapeva <sofia_lapteva@mail.ru>
2022-06-25 20:05:19 +0300

    added accordeon

--- src/pages/Cheetsheets.jsx
+++ src/pages/Cheetsheets.jsx
@@ -68,45 +68,49 @@ export default function Cheetsheets() {
     return (
         <>
             <Header />
-            <div className='w-8/12 mx-auto'>
-                <div className="flex flex-col items-start justify-start">
-                    <h2 className="mt-16 text-lg font-semibold text-gray-900">
-                        Шпаргалка по тегам HTML
-                    </h2>
-                    <p className="mt-2 text-sm text-gray-700">
-                        По материалам <a href="https://doka.guide" rel="noreferrer" target={"_blank"} className='text-blue-600 hover:underline'>doka.guide</a>
-                    </p>
-                </div>
-                <div className="mt-8 flex flex-col">
-                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                                <table className="min-w-full divide-y divide-gray-300">
-                                    <thead className="bg-gray-50">
-                                        <tr>
-                                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                                Название
-                                            </th>
-                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                Назначение
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="divide-y divide-gray-200 bg-white">
-                                        {tagNames.map((name, i) => (
-                                            <tr key={name}>
-                                                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                                                    {name}
-                                                </td>
-                                                <td className=" px-3 py-4 text-sm text-gray-500">{tagSemantics[i]}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
+            <div className='w-8/12 lg:w-6/12 mx-auto'>
+                <details>
+                    <summary className="flex flex-col items-start justify-start cursor-pointer bg-gray-50 hover:bg-gray-100 py-4 px-6">
+                        <li className="text-lg font-semibold text-gray-900">
+                            Шпаргалка по тегам HTML
+                        </li>
+                        <p className="mt-2 text-sm text-gray-700">
+                            По материалам <a href="https://doka.guide" rel="noreferrer" target={"_blank"} className='text-blue-600 hover:underline'>doka.guide</a>
+                        </p>
+                    </summary>
+                    <div>
+                        <div className="mt-8 flex flex-col">
+                            <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                    <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                        <table className="min-w-full divide-y divide-gray-300">
+                                            <thead className="bg-gray-50">
+                                                <tr>
+                                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                                        Название
+                                                    </th>
+                                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                        Назначение
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody className="divide-y divide-gray-200 bg-white">
+                                                {tagNames.map((name, i) => (
+                                                    <tr key={name}>
+                                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                                            {name}
+                                                        </td>
+                                                        <td className=" px-3 py-4 text-sm text-gray-500">{tagSemantics[i]}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </details>
             </div>
         </>
     )
